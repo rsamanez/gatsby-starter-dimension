61cc707052c4e27b87f8e733cdb2beee650e68c0
Rommel Samamnez <rsamanez@gmail.com>
2019-12-15 23:54:53 -0500

    Title update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Personal Web Page",
+    author: "Rommel Samanez",
+    description: "This is my First Personal Web Page."
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'gatsby-web-page',
+        short_name: 'myweb',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#0B152E',
+        theme_color: '#0B152E',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
