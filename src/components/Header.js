2dceadfcec480a6bd88cc5a485b8361cd3dcee15
Rommel Samamnez <rsamanez@gmail.com>
2019-12-15 21:23:21 -0500

    new design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import photo from '../images/photo.jpg'
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-diamond"></span>
+                <span className="image fit">
+                    <img src={photo} alt="" />
+                </span>
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Rommel Samanez</h1>
+                <p>Computer Engineer</p>
             </div>
         </div>
         <nav>
